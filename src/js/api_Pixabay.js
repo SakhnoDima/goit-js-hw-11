d390d1e1ee11d0a3091903a658f975794bb3cbb2
SakhnoDima <134371251+SakhnoDima@users.noreply.github.com>
2023-07-31 18:46:14 +0300

    1

--- src/js/api_Pixabay.js
+++ src/js/api_Pixabay.js
@@ -3,13 +3,22 @@ Notify.init({
     timeout: 3000,   
     position: 'left-top',
 })
+const axios = require('axios').default;
+
 
 export function fetchPhoto(serch_valie){
     const BASE_URL = "https://pixabay.com/api/";
-    const MY_KEY = "?key=38565810-29740f5778639307be3f3659"
-    const url = `${BASE_URL}${MY_KEY}&q=${serch_valie}&image_type=photo&orientation=horizontal&safesearch=true`;
+    const MY_KEY = "?key=38565810-29740f5778639307be3f3659c"
+    // const url = `${BASE_URL}${MY_KEY}&q=${serch_valie}&image_type=photo&orientation=horizontal&safesearch=true`;
     
-    return fetch(url).then(res=>{
-        if (!res.ok) throw new Error(res.status)
-       return res.json()})
+
+    return axios({
+        method: 'get',
+        url : `${BASE_URL}${MY_KEY}&q=${serch_valie}&image_type=photo&orientation=horizontal&safesearch=true`,
+      })
+        .then(response =>  response)
+
+    // return fetch(url).then(res=>{
+    //     if (!res.ok) throw new Error(res.status)
+    //    return res.json()})
     }
